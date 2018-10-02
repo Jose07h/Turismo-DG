f9e83282dab7e624c00085903901f28e08427306
MiquiztliMal <isc_salgado.m@tesvb.edu.mx>
2018-10-02 00:29:45 -0500

    login_view

--- Config/libs/Request.php
+++ Config/libs/Request.php
@@ -1,40 +1,62 @@
 <?php
 
-
 namespace AppData\Config;
 
+use AppData\Model\login;
 
 class Request
 {
     private $controlador;
     private $metodo;
     private $argumento;
-
+//    private $log=URL.Login;
     public function __construct()
     {
-        if (isset($_GET['url']))
+        if (isset($_SESSION["username"]))
         {
-            $ruta = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_URL);
-            $ruta = explode("/", $ruta);
-            $ruta = array_filter($ruta);
-            //print_r($ruta); //sirve para imprimir arreglos
-            if ($ruta[0] == "index.php")
-            {
-                $this->controlador = "inicio";
+            if (isset($_GET['url'])) {
+                $ruta = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_URL);
+                $ruta = explode("/", $ruta);
+                $ruta = array_filter($ruta);
+                //print_r($ruta); //sirve para imprimir arreglos
+                if ($ruta[0] == "index.php") {
+                    $this->controlador = "empleado_bienvenido";
+                } else {
+                    $this->controlador = strtolower(array_shift($ruta));
+                }
+                $this->metodo = strtolower(array_shift($ruta));
+                if (!$this->metodo)
+                    $this->metodo = "index";
+                $this->argumento = $ruta;
+            }else {
+                $this->controlador = "empleado_bienvenido";
+                $this->metodo = "index";
             }
+      }
+      else
+          if (isset($_GET['url'])?stristr($_GET['url'],'login'):false)
+        {
+            $this->controlador="login";
+
+
+            if(isset($_POST["email"]))
+
+                $this->metodo = "verify";
+
             else
-            {
-                $this->controlador = strtolower(array_shift($ruta));
-            }
-            $this->metodo=strtolower(array_shift($ruta));
-            if (!$this->metodo)
-                $this->metodo="index";
-            $this->argumento=$ruta;
+                $this->metodo = "index";
+        }
+        else if (isset($_GET['url'])?stristr($_GET['url'],'ReservacionesCliente'):false)
+        {
+            $this->controlador="ReservacionesCliente";
+
+            $this->metodo=stristr($_GET['url'],'consulta')?"consulta":"index";
         }
         else
         {
-            $this->controlador = "inicio";
+            $this->controlador="inicio";
             $this->metodo = "index";
+
         }
 
     }
