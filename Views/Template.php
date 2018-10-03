972af5fa9516bf051b7c7af726d791496dddd012
JoseHd <joseluis07h@gmail.com>
2018-10-03 17:55:42 -0500

    Reparaciones

--- Views/Template.php
+++ Views/Template.php
@@ -26,8 +26,9 @@ class Template
         </head>
         <body>
         <header>
+
           <nav class="navbar navbar-expand-lg navbar-light ">
-          <a class="navbar-brand" href="#">Campamento Malacatepec</a>
+          <a class="navbar-brand" href="#">Campamento Malacatepec..</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
