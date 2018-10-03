625d7ebd7d095eac3b151a254573fa170194d0b0
JoseHd <joseluis07h@gmail.com>
2018-10-03 17:51:07 -0500

    Reparaciones

--- Views/Template.php
+++ Views/Template.php
@@ -9,14 +9,13 @@ class Template
         <!DOCTYPE html>
         <html>
         <head>
+          <title>Principal</title>
             <meta charset="utf-8">
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
             <link rel="stylesheet" href="<?php echo URL?>Public/css/bootstrap.min.css">
-            
-            <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+            <script src="<?php echo URL?>Public/js/jquery-3.3.1.slim.min.js" ></script>
             <script src="<?php echo URL?>Public/js/bootstrap.min.js" ></script>
+            <link rel="stylesheet" href="<?php echo URL?>Public/css/style.css">
             <script>
               $(document).on("ready",function(){
               $('.carousel').carousel({
@@ -24,119 +23,64 @@ class Template
               })
             });
             </script>
-
-
-
-        
         </head>
-        <title>Principal</title>
         <body>
-       <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">Navbar</a>
+        <header>
+          <nav class="navbar navbar-expand-lg navbar-light ">
+          <a class="navbar-brand" href="#">Campamento Malacatepec</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="<?php echo URL?>inicio">Inicio<span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="<?php echo URL?>Login">Login</a>
               </li>
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Dropdown
-                </a>
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Otras</a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="#">Action</a>
-                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Guias</a>
+                  <a class="dropdown-item" href="#">Actividades</a>
                   <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">Something else here</a>
+                  <a class="dropdown-item" href="#">Algo mas</a>
                 </div>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
               </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
-              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+              <input class="form-control mr-sm-2" type="Buscar" placeholder="Buscar" aria-label="Buscar">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
             </form>
           </div>
-        </nav>
-      </header>
-      <div class="container md-8">
-          <div class="row">
-            <div class="col-md-10">
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                      <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-                      <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-                      
-                      
-                    </ol>
-                    <div class="carousel-inner">
-                      <div class="carousel-item active">
-                        <img class="d-block w-100" src="<?php echo URL?>Public/img/1.jpg" alt="First slide">
-                      </div>
-                      <div class="carousel-item">
-                        <img class="d-block w-100" src="<?php echo URL?>Public/img/2.jpg" alt="Second slide">
-                      </div>
-                      <div class="carousel-item">
-                        <img class="d-block w-100" src="<?php echo URL?>Public/img/3.jpg" alt="Third slide">
-                      </div>
-                      <div class="carousel-item">
-                        <img class="d-block w-100" src="<?php echo URL?>Public/img/4.jpg" alt="Third slide">
-                      </div>
-                      <div class="carousel-item">
-                        <img class="d-block w-100" src="<?php echo URL?>Public/img/5.jpg" alt="Third slide">
-                      </div>
-                      
-
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                      <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                      <span class="sr-only">Next</span>
-                    </a>
-                </div>    
-              </div>
-          </div>
-        </div>
-        <main class="container">
-
-        <?php
+          </nav>
+        </header>
+        <main class="container md-14">
+          
+      
+    <?php
     }
     public static function footer()
     {
-        /*echo "footer de mi proyecto";*/
-        ?>
+    ?>
         </main>
-            <footer>
-                <ul class="nav">
-                  <li class="nav-item">
-                    <a class="nav-link active" href="#">Active</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                  </li>
-                </ul>
-              </footer> 
+          <footer>
+            <ul class="nav">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Contáctanos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Nuestras redes</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Ubicacion</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#">Quejas y sugerencias</a>
+              </li>
+            </ul>
+          </footer> 
         </body>
         </html>
         <?php
