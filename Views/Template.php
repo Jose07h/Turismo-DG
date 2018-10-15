601772c7dd8de3b3dc909fdb21bee20ddf5c7a12
MiquiztliMal <isc_salgado.m@tesvb.edu.mx>
2018-10-14 22:29:20 -0500

    corrección de boton

--- Views/Template.php
+++ Views/Template.php
@@ -31,38 +31,45 @@ class Template
           </head>
         <body>
         <header>
-          <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-white flex-md-nowrap p-0 shadow">
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">                
-                <img src="<?php echo URL?>Public/img/mexico.jpg" width="250px" alt="Thumbnail Image" class="img-raised  img-fluid" >
-              </li>
-              <li class="nav-item active">
-                    <a class="nav-link" href="<?php echo URL?>inicio"> <font color="black" face="Arial,Verdana" size="9"  > Conociendo Donato Guerra <span class="sr-only">(current)</span></font></a>
-              </li>
 
-
-                <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Otras</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="#">Guias</a>
-                  <a class="dropdown-item" href="#">Actividades</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">Algo mas</a>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <img src="<?php echo URL?>Public/img/logo_trans.png" width="150px" alt="Thumbnail Image" class="img-raised  img-fluid">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="<?php echo URL?>inicio"> <font color="#fafad2" face="Arial,Verdana" size="5"> Conociendo Donato Guerra <span class="sr-only">(current)</span></font></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Costumbres y tradiciones</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Artesanias</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Login</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Actividades
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Campismo</a>
+                                <a class="dropdown-item" href="#">Ciclismo</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Buscar..." aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+                    </form>
                 </div>
-              </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input type="text" class="form-control" id="buscar" placeholder="buscar">
-                <a class="nav-link" href="#">
-                  <button class="" data-feather="search"></button>
-                </a>
-            </form>
-          </div>
-          </nav>
+            </nav>
+
         </header>
         <main class="container-fluid md-12">
     <?php
